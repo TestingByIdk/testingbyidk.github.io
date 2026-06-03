0672c97e8c3f28e5dcbe380bc6b2854830b61682
TestingByIdk <149605205+TestingByIdk@users.noreply.github.com>
2026-06-02 22:57:42 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 console.log("SCRIPT LOADED");
 
-document.addEventListener("DOMContentLoaded", () => {
-    alert("JavaScript is working!");
+document.addEventListener("DOMContentLoaded", function () {
+
+    const accordions = document.querySelectorAll(".accordion");
+
+    accordions.forEach(function(button) {
+
+        button.addEventListener("click", function() {
+
+            const panel = this.nextElementSibling;
+
+            if (panel.style.display === "block") {
+                panel.style.display = "none";
+            } else {
+                panel.style.display = "block";
+            }
+
+        });
+
+    });
+
 });
