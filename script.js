8714beb0203a4e55429cd1c83ebe542f4d67995f
TestingByIdk <149605205+TestingByIdk@users.noreply.github.com>
2026-06-02 22:50:56 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+document.addEventListener("DOMContentLoaded", () => {
 
+    const accordions = document.querySelectorAll(".accordion");
+
+    accordions.forEach(button => {
+
+        button.addEventListener("click", () => {
+
+            const panel = button.nextElementSibling;
+
+            if (panel.style.display === "block") {
+                panel.style.display = "none";
+            } else {
+                panel.style.display = "block";
+            }
+
+        });
+
+    });
+
+});
